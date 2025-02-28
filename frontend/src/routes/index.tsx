0bb9f46610426e1250b0b4ba3948315207f90645
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-02-28 20:55:28 +0100

    clean: remove all unnecessary stuff

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,14 +5,7 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1 class="text-2xl">Hello world!</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      
     </main>
   );
 }
